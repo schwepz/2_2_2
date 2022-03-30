4f54343c5a0725dd0e82d6a28efea5082ed82a38
Administrator <kirillschwepz@gmail.com>
2022-03-30 15:41:51 +0700

    30 03 2022 1541 гит.

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Controller
 public class HelloController {
 
+
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
